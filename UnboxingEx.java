466f6fbdc57798393b98e664905d18824b0baadc
ankur <ankurkumar0924@gmail.com>
2025-02-14 01:07:50 +0530

    Unboxing in java

---
+++ UnboxingEx.java
@@ -0,0 +1,13 @@
+package Wrapperclass;
+
+public class UnboxingEx {
+
+	public static void main(String[] args) 
+	{
+		Integer x = new Integer(20);
+		 int a = x;
+		 System.out.println(a);
+
+	}
+
+}
