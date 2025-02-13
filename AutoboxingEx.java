d3f09aa9b73d67a6ddd209eb8d188288896fb2f8
ankur <ankurkumar0924@gmail.com>
2025-02-14 00:48:20 +0530

    First java repository

---
+++ AutoboxingEx.java
@@ -0,0 +1,14 @@
+package Wrapperclass;
+
+public class AutoboxingEx {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		Integer b = new Integer(10);
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
