1b7e90826f3a9a22f22a1c99556753fd1bba09c8
ankur <ankurkumar0924@gmail.com>
2025-02-24 17:10:52 +0530

    exception handling

---
+++ CheckedUncheckedEx2.java
@@ -0,0 +1,12 @@
+package exceptionHandling;
+
+public class CheckedUncheckedEx2 
+{
+	public static void main(String[] args) 
+	{
+		int a = 5 , b = 0 ;
+		int c = a/b ;        // unchecked Exception
+		System.out.println(c);	
+	}
+
+}
