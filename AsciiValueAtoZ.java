173bcd87dae6eb28b316bbf2091d9fc2e2f25952
ankur <ankurkumar0924@gmail.com>
2025-03-03 18:08:38 +0530

    ASCII value from A to Z

---
+++ AsciiValueAtoZ.java
@@ -0,0 +1,17 @@
+package QuestionsPractice;
+
+public class AsciiValueAtoZ
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("ASCII VALUE OF A to Z");
+			
+		for(char ch ='A'; ch<='z'; ch++)
+		{
+			//int val = ch;
+			System.out.println(ch + " = " + (int)ch);
+		}
+		
+	}
+
+}
