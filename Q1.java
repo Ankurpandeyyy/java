b014209fc5f188f8a437aa4b0b9dbf21bf08d956
ankur <ankurkumar0924@gmail.com>
2025-03-03 18:10:56 +0530

    Some_Concepts

---
+++ Q1.java
@@ -0,0 +1,12 @@
+package QuestionsPractice;
+
+public class Q1 {
+	// int x;   // instance member variable---it is non-static so it can't be accessed without making object in main class
+	static int x;
+	public static void main(String[] args) {
+		
+		System.out.println(x);
+
+	}
+
+}
