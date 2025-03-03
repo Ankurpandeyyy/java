90b12f20b9dda7dbca95c01feed5d4f726d1be33
ankur <ankurkumar0924@gmail.com>
2025-03-03 18:13:40 +0530

    Some_Concepts

---
+++ Q2_concept.java
@@ -0,0 +1,17 @@
+package QuestionsPractice;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		//int x;  // we cannot use a local variable without initialising as it cannot have default value as 0( it has blank as default)
+		int x=5;
+		//float y = 3.5; // we cannot use this as it is double and can't be converted to float without f
+		float y= 3.5f;
+		//static int x1; // we cannot take static variable inside main method OR no any concept of static local variable in java 
+		System.out.println(x);
+		System.out.println(y);
+		
+
+	}
+
+}
