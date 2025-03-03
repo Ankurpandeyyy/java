ad25e6d4d7cf9735a246823739f29ad5cccd7129
ankur <ankurkumar0924@gmail.com>
2025-03-03 18:15:05 +0530

    Some_Concepts

---
+++ Q4_concept.java
@@ -0,0 +1,12 @@
+package QuestionsPractice;
+
+public class Q4
+{
+	int a=10; // without object we cannot use instance variable in java
+
+public static void main(String args[])
+{
+	//Q4 ex; // this is not a object in java as we use new keyword for object---here it is a blank local variable "ex"
+	//System.out.println(ex.a);  // we can't use local variable without initialising
+}
+}
