5423030273e2903fb21edc2e22f9c49b35fa137e
ankur <ankurkumar0924@gmail.com>
2025-03-03 18:17:00 +0530

    Some_Concepts

---
+++ Q5_concept.java
@@ -0,0 +1,17 @@
+package QuestionsPractice;
+class Dummy
+{
+	int a=10;
+}
+
+public class Q5 {
+
+	public static void main(String[] args) 
+	{
+	//	Q5 ex = new Dummy();  // incompatible type
+		Dummy ex = new Dummy();
+		System.out.println(ex.a);
+
+	}
+
+}
