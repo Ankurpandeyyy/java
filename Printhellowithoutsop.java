a1dbc0f1f4bc1890f00ac33a85a19fa1074192da
ankur <ankurkumar0924@gmail.com>
2025-03-04 23:57:51 +0530

    Some_Concepts

---
+++ Printhellowithoutsop.java
@@ -0,0 +1,16 @@
+package trialcodes;
+
+public class Printhellowithoutsop {
+
+	public static void main(String[] args)
+	{
+		//System.out.println(System.out.printf("hello"));
+        
+		if(System.out.printf ("hello")==null)	
+		{
+			
+		}
+	     
+		
+	}
+}
