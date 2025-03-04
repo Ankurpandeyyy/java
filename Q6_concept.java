cab9ab480d7ee970570bbf078fa76c5faf74d811
ankur <ankurkumar0924@gmail.com>
2025-03-05 02:16:25 +0530

    Some_Concepts

---
+++ Q6_concept.java
@@ -0,0 +1,22 @@
+package QuestionsPractice;
+class Dummy2
+{
+	private int a=10;
+	 int b=23;
+	 public void show()
+	 {
+		 System.out.println(a);
+	 }
+}
+
+public class Q6 {
+
+	public static void main(String[] args)
+	{
+		Dummy2 d = new Dummy2();
+		d.show();
+		//System.out.println(d.a);  
+	  System.out.println(d.b);
+	}
+
+}
