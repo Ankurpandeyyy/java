949b3fb59fa2f6250603eba1c60390aa1a770fcf
ankur <ankurkumar0924@gmail.com>
2025-03-05 02:17:38 +0530

    Some_Concepts

---
+++ Q8_concept.java
@@ -0,0 +1,17 @@
+package QuestionsPractice;
+
+public class Q8 {
+
+	public static void main(String[] args)
+	{
+		Q8 x = new Q8();
+		//Q8.f1();
+
+	}
+	public void f1()
+	{
+		//static int a;   //no any concept of static local variable
+	//	System.out.println(f1.a);
+	}
+
+}
