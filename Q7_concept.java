7677227cff39a7272bb40701ded2bba1f79b7327
ankur <ankurkumar0924@gmail.com>
2025-03-05 02:16:58 +0530

    Some_Concepts

---
+++ Q7_concept.java
@@ -0,0 +1,18 @@
+package QuestionsPractice;
+class Random
+{
+	//public final int a ;
+}
+
+public class Q7
+{
+
+	public static void main(String[] args)
+
+	{
+		Random x = new Random();
+		//System.out.println(x.a);
+
+	}
+
+}
