2fe51af68c99e628a6cf7fa00b320faba3c99e11
ankur <ankurkumar0924@gmail.com>
2025-05-14 18:47:45 +0530

    JAVA Swings

---
+++ Ex_01.java
@@ -0,0 +1,15 @@
+package Swing_Practice;
+import javax.swing.*;
+public class Ex_01 {
+
+	public static void main(String[] args)
+	{
+		JFrame j = new JFrame();
+		j.setVisible(true);
+		j.setSize(500,500);
+		j.setTitle("Newww");
+		//j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+	}
+
+}
